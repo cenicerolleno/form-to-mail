23481baf9a807c842abddcf4d48f9264e5188a8a
cenicerolleno <mrnolan88@gmail.com>
2026-08-08 01:34:18 +0200

    feat: actualizacion config.py con variables de entorno

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     DEBUG = True
+    BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+    MAIL_FROM = os.getenv("MAIL_FROM")
+    MAIL_TO = os.getenv("MAIL_TO")
